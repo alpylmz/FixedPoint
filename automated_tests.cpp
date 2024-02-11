c3199b95c461c5c66bc17e8974021b9c4fbb53bb
alpylmz <ylmz.alp.e@gmail.com>
2024-02-11 12:54:58 -0500

    slightly improved tester

--- automated_tests.cpp
+++ automated_tests.cpp
@@ -4,7 +4,20 @@
 #include "fixed_point.hpp"
 #include "bad.hpp"
 
+#define CURR_TYPE FixedPoint<20,20>
 
+//OverflowMode number_mode = OverflowMode::CAP_MODE;
+const double precision = 0.01;
+
+bool currtype_equal(CURR_TYPE a, CURR_TYPE b){
+    // If they are close enough
+    return (a - b) < CURR_TYPE(precision) && (b - a) < CURR_TYPE(precision);
+}
+
+bool currtype_equal(CURR_TYPE a, double b){
+    // If they are close enough
+    return (a - b) < CURR_TYPE(precision) && (b - a) < CURR_TYPE(precision);
+}
 
 int main(){
     // start reading tests.txt
@@ -22,10 +35,11 @@ int main(){
             }
             case '#':{// Assignment test
                 infile >> num1 >> num2;
-                auto p1 = (FixedPoint<8,8>*)get_type(n, m);
+                //auto p1 = (CURR_TYPE)get_type(n, m);
+                CURR_TYPE* p1 = new CURR_TYPE;
                 *p1 = num1;
                 // now need to check if they are equal
-                if(*p1 == num2){
+                if(currtype_equal(*p1, num2)){
                     outfile << "OK " << num1 << " == " << num2 << " " << *p1 << std::endl;
                 } else {
                     outfile << "FAIL " << num1 << " != " << num2 << " " << *p1 << std::endl;
@@ -34,12 +48,14 @@ int main(){
             }
             case '+':{// Addition test
                 infile >> num1 >> num2 >> num3;
-                auto p1 = (FixedPoint<8,8>*)get_type(n, m);
+                //auto p1 = (CURR_TYPE)get_type(n, m);
+                CURR_TYPE* p1 = new CURR_TYPE;
                 *p1 = num1;
-                auto p2 = (FixedPoint<8,8>*)get_type(n, m);
+                //auto p2 = (CURR_TYPE)get_type(n, m);
+                CURR_TYPE* p2 = new CURR_TYPE;
                 *p2 = num2;
                 // now need to check if they are equal
-                if(*p1 + *p2 == num3){
+                if(currtype_equal(*p1 + *p2, num3)){
                     outfile << "OK " << num1 << " + " << num2 << " == " << num3 << "==" << *p1 + *p2 << std::endl;
                 } else {
                     outfile << "FAIL " << num1 << " + " << num2 << " == " << *p1 << " + " << *p2 << " == " << *p1+*p2 << " != " << num3  << std::endl;
@@ -48,9 +64,11 @@ int main(){
             }
             case '-':{// Subtraction test
                 infile >> num1 >> num2 >> num3;
-                auto p1 = (FixedPoint<8,8>*)get_type(n, m);
+                //auto p1 = (CURR_TYPE)get_type(n, m);
+                CURR_TYPE* p1 = new CURR_TYPE;
                 *p1 = num1;
-                auto p2 = (FixedPoint<8,8>*)get_type(n, m);
+                // auto p2 = (CURR_TYPE)get_type(n, m);
+                CURR_TYPE* p2 = new CURR_TYPE;
                 *p2 = num2;
                 // now need to check if they are equal
                 if(*p1 - *p2 == num3){
@@ -62,9 +80,11 @@ int main(){
             }
             case '*':{// Multiplication test
                 infile >> num1 >> num2 >> num3;
-                auto p1 = (FixedPoint<8,8>*)get_type(n, m);
+                //auto p1 = (CURR_TYPE)get_type(n, m);
+                CURR_TYPE* p1 = new CURR_TYPE; 
                 *p1 = num1;
-                auto p2 = (FixedPoint<8,8>*)get_type(n, m);
+                //auto p2 = (CURR_TYPE)get_type(n, m);
+                CURR_TYPE* p2 = new CURR_TYPE;
                 *p2 = num2;
                 // now need to check if they are equal
                 if(*p1 * *p2 == num3){
